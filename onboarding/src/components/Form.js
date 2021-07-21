b8350f4159b6ce525f26a9130490554a0be4cf9c
Jamaria Sims <jamariasims@Jamarias-MacBook-Air.local>
2021-07-21 07:27:17 -0400

    Start

---
+++ onboarding/src/components/Form.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Form() {
+	{
+		return (
+			<div>
+				<label>
+					Name <input type="text" />
+				</label>
+				<label>
+					Email <input type="text" />
+				</label>
+				<label>
+					Password <input type="text" />
+				</label>
+				<label>Terms of Service</label>
+				<button>Name</button>
+			</div>
+		);
+	}
+}
