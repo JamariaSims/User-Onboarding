8072e04d54b0985279c11bcc269c6d38961bddb7
Jamaria Sims <jamariasims@Jamarias-MacBook-Air.local>
2021-07-23 02:19:47 -0400

    Done

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,11 +1,26 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Form from "./components/Form";
 
 function App() {
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		axios
+			.get(`https://reqres.in/api/users`)
+			.then((res) => setData(res.data.data));
+	}, []);
 	return (
 		<div className="App">
-			<Form />
+			<h1>OnBoard</h1>
+			<Form data={data} setData={setData} />
+			{data &&
+				data.map((x, y) => (
+					<>
+						<p>{JSON.stringify(x)}</p>
+					</>
+				))}
+			*
 		</div>
 	);
 }
--- onboarding/src/components/Form.js
+++ onboarding/src/components/Form.js
@@ -1,21 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+const initialData = {
+	name: "",
+	email: "",
+	password: "",
+	termOfService: false,
+};
 
-export default function Form() {
-	{
-		return (
-			<div>
-				<label>
-					Name <input type="text" />
+export default function Form(props) {
+	const { data, setData } = props;
+	const [form, setForm] = useState(initialData);
+	const handleChange = (e) => {
+		const { value } = e.target;
+		setForm({ ...form, [e.target.name]: value });
+	};
+	const submitForm = (e) => {
+		e.preventDefault();
+		if (!form.name || !form.email || !form.password) return;
+		debugger;
+		axios.post(`https://reqres.in/api/users`, form).then((res) => {
+			console.log(data);
+			setData([...data, res.data]);
+			setForm(initialData);
+		});
+	};
+	return (
+		<div className="formInput">
+			<form onSubmit={submitForm}>
+				<label htmlFor="name">Name</label>
+				<input
+					type="text"
+					name="name"
+					value={form.name}
+					onChange={handleChange}
+				/>
+				<label htmlFor="email">Email</label>
+				<input
+					type="text"
+					name="email"
+					value={form.email}
+					onChange={handleChange}
+				/>
+				<label htmlFor="password">Password</label>
+				<input
+					type="text"
+					name="password"
+					value={form.password}
+					onChange={handleChange}
+				/>
+				<label htmlFor="termsOfService">
+					Terms of Service
+					<input
+						type="radio"
+						name="termOfService"
+						checked={form.termOfService}
+						onChange={handleChange}
+					/>
 				</label>
-				<label>
-					Email <input type="text" />
-				</label>
-				<label>
-					Password <input type="text" />
-				</label>
-				<label>Terms of Service</label>
-				<button>Name</button>
-			</div>
-		);
-	}
+				<br />
+				<button>Submit</button>
+			</form>
+		</div>
+	);
 }
